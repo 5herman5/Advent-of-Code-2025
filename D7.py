10d410247bc0f9591ce2148c749b15fef31c50de
5herman5 <baileypclarke@gmail.com>
2025-12-07 20:27:51 +1100

    Made it a bit nicer

--- D7.py
+++ D7.py
@@ -4,18 +4,18 @@ s = f.read()
 l = [line for line in s.split("\n")]
 
 _splits = set()
-_dict2 = {}
+_timelines = {}
 
 def timelines(n, l, x):
-    if (n, x) not in _dict2:
+    if (n, x) not in _timelines:
         if n == len(l):
-            _dict2[n, x] = 1
+            _timelines[n, x] = 1
         elif l[n][x] == ".":
-            _dict2[n, x] = timelines(n+1, l, x)
+            _timelines[n, x] = timelines(n+1, l, x)
         else:
-            _dict2[n, x] = timelines(n+1, l, x-1) + timelines(n+1, l, x+1)
+            _timelines[n, x] = timelines(n+1, l, x-1) + timelines(n+1, l, x+1)
             _splits.add((n, x))
 
-    return _dict2[n, x]
+    return _timelines[n, x]
 
 print(timelines(1, l, l[0].find("S")), len(_splits))
